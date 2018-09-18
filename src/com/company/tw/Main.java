b91e5d492871f12044754ec8e2f265a144a049b2
eMe-404 <838702939>
2018-09-18 23:54:19 +0800

    [yaer] feat : Draw a vertical line

--- src/com/company/tw/Main.java
+++ src/com/company/tw/Main.java
@@ -3,14 +3,10 @@ package com.company.tw;
 public class Main {
 
     public static void main(String[] args) {
-        int n = 8;
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i <= n; i++) {
-            result.append("*");
+        int n = 3;
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
         }
-
-        System.out.println(result);
 
     }
 }
