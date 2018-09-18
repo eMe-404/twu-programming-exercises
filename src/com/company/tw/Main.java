4dd3444cb774d891c0c94c988ad09bf3bae817cb
eMe-404 <838702939>
2018-09-19 00:27:53 +0800

    [yaer] feat : Diamond with Name

--- src/com/company/tw/Main.java
+++ src/com/company/tw/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         int n = 3;
-        for (int i = 1; i <= n; i++) {
+        String name = "yaer";
+
+        for (int i = 1; i < n; i++) {
             printOneLine(n, i);
         }
+
+        System.out.println(name);
+
         for (int i = n - 1; i > 0; i--) {
             printOneLine(n, i);
         }
