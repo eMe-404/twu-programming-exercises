805a9660ef875c1b71b6f14ca01564b016c3306e
eMe-404 <838702939>
2018-09-19 00:17:34 +0800

    [yaer] feat : Isosceles Triangle

--- src/com/company/tw/Main.java
+++ src/com/company/tw/Main.java
@@ -5,16 +5,24 @@ public class Main {
     public static void main(String[] args) {
         int n = 3;
         for (int i = 1; i <= n; i++) {
-            System.out.println(generateAsterisk(i));
+            final int asteriskNum = i * 2 - 1;
+            final int backspaceNum = n - i;
+            System.out.println(generateOneLine(asteriskNum, backspaceNum));
         }
 
     }
 
-    private static StringBuilder generateAsterisk(int num) {
+    private static StringBuilder generateOneLine(int asteriskNum, int backspaceNum) {
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < num; i++) {
+
+        for (int i = 0; i < backspaceNum; i++) {
+            result.append(" ");
+        }
+
+        for (int i = 0; i < asteriskNum; i++) {
             result.append("*");
         }
+
         return result;
     }
 
