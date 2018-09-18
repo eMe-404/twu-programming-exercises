d11b2a82898ece4d0e7f57954fc603355df5cac7
eMe-404 <838702939>
2018-09-18 23:48:49 +0800

    [yaer] feat : Draw a horizontal line

---
+++ src/com/company/tw/Main.java
@@ -0,0 +1,12 @@
+package com.company.tw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 8;
+        for (int i = 0; i <= n; i++) {
+            System.out.println("*");
+        }
+
+    }
+}
