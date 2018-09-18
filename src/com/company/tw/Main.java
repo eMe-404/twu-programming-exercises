864791c6603d3ba15ee7be63e2e5a15c25c4dad9
eMe-404 <838702939>
2018-09-18 23:52:51 +0800

    [yaer] fix : Draw a horizontal line

--- src/com/company/tw/Main.java
+++ src/com/company/tw/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         int n = 8;
+        StringBuilder result = new StringBuilder();
+
         for (int i = 0; i <= n; i++) {
-            System.out.println("*");
+            result.append("*");
         }
+
+        System.out.println(result);
 
     }
 }
