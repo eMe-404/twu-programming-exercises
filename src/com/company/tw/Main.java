75cdc8e233273a4920ca3e1ac370c495d7d60133
eMe-404 <838702939>
2018-09-19 00:00:38 +0800

    [yaer] feat : Draw a right triangle

--- src/com/company/tw/Main.java
+++ src/com/company/tw/Main.java
@@ -4,9 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         int n = 3;
-        for (int i = 0; i < n; i++) {
-            System.out.println("*");
+        for (int i = 1; i <= n; i++) {
+            System.out.println(generateAsterisk(i));
         }
 
     }
+
+    private static StringBuilder generateAsterisk(int num) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            result.append("*");
+        }
+        return result;
+    }
+
 }
