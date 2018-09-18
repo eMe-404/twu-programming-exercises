1741a339235a8b71dcf8e06340c930cba4d3cd5f
eMe-404 <838702939>
2018-09-19 00:51:57 +0800

    [yaer] feat : FizzBuzz Exercise

--- src/com/company/tw/Main.java
+++ src/com/company/tw/Main.java
@@ -3,39 +3,25 @@ package com.company.tw;
 public class Main {
 
     public static void main(String[] args) {
-        int n = 3;
-        String name = "yaer";
-
-        for (int i = 1; i < n; i++) {
-            printOneLine(n, i);
-        }
-
-        System.out.println(name);
-
-        for (int i = n - 1; i > 0; i--) {
-            printOneLine(n, i);
-        }
-
+        int num = 100;
+        fizzBuzz(num);
     }
 
-    private static void printOneLine(int n, int i) {
-        final int asteriskNum = i * 2 - 1;
-        final int backspaceNum = n - i;
-        System.out.println(generateOneLine(asteriskNum, backspaceNum));
-    }
+    private static void fizzBuzz(int num) {
+        for (int i = 1; i <= num; i++) {
+            StringBuilder outputStr = new StringBuilder();
+            if (i % 3 == 0) {
+                outputStr.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                outputStr.append("Buzz");
+            }
+            if (outputStr.length() == 0) {
+                outputStr.append(i);
+            }
 
-    private static StringBuilder generateOneLine(int asteriskNum, int backspaceNum) {
-        StringBuilder result = new StringBuilder();
+            System.out.println(outputStr);
 
-        for (int i = 0; i < backspaceNum; i++) {
-            result.append(" ");
         }
-
-        for (int i = 0; i < asteriskNum; i++) {
-            result.append("*");
-        }
-
-        return result;
     }
-
 }
